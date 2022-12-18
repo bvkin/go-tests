31af4f4998da0ad3927d4b2282134225fc452a2d
Benjamin Kincaid <ben.kincaid@rocketinsights.com>
2022-12-18 11:49:51 -0500

    concurrency single threaded setup

--- mocking/main.go
+++ mocking/main.go
@@ -9,31 +9,58 @@ import (
 
 const finalWord = "Go!"
 const countdownStart = 3
+const write = "write"
+const sleep = "sleep"
 
 type Sleeper interface {
 	Sleep()
 }
-type SpySleeper struct {
-	Calls int
-}
-func (s *SpySleeper) Sleep() {
-	s.Calls++
-}
-type DefaultSleeper struct{}
 
+// Default Sleeper
+type DefaultSleeper struct{}
 func (d *DefaultSleeper) Sleep() {
 	time.Sleep(1 * time.Second)
 }
+
+// SpyCountdownOperations
+type SpyCountdownOperations struct {
+	Calls []string
+}
+func (s *SpyCountdownOperations) Sleep() {
+	s.Calls = append(s.Calls, sleep)
+}
+func (s *SpyCountdownOperations) Write(p []byte) (n int, err error) {
+	s.Calls = append(s.Calls, write)
+	return
+}
+
+// Configurable Sleeper
+type ConfigurableSleeper struct {
+	duration time.Duration
+	sleep    func(time.Duration)
+}
+func (c *ConfigurableSleeper) Sleep() {
+	c.sleep(c.duration)
+}
+type SpyTime struct {
+	durationSlept time.Duration
+}
+func (s *SpyTime) Sleep(duration time.Duration) {
+	s.durationSlept = duration
+}
+
+
 
 func Countdown(out io.Writer, sleeper Sleeper) {
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 		sleeper.Sleep()
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
 func main(){
-	sleeper := &DefaultSleeper{}
+	sleeper := &ConfigurableSleeper{1 * time.Second, time.Sleep}
 	Countdown(os.Stdout, sleeper)
 }
